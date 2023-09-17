db6ca5f430ff17a4b3efcd6222d9e2b151667a60
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-09-17 14:52:26 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main{
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try {
+            String apiUrl = "https://api.example.com/data"; 
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            // Get the response code from the server
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // Read the response data from the server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+            reader.close();
+
+            // Print the response data
+            System.out.println("Response Data:");
+            System.out.println(response.toString());
+
+            // Close the connection
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
