3ae6535474830f7fe428d6a0166d4d74f67ff5b8
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-09-24 15:21:51 -0400

    style

--- Application/application/src/App.js
+++ Application/application/src/App.js
@@ -1,24 +1,36 @@
 import EnergyMonitor from "./Charts/EnergyMonitor";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App" style={{ backgroundColor: "black" }}>
-      <div style={{ textAlign: "center", fontSize: "24px", paddingTop: "30px", color:"white"}}>
-        Remote Energy Monitoring System
-      </div>
-
+    return (
       <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
+        className="App"
+        style={{ backgroundColor: "black", position: "relative" }}
       >
-        <EnergyMonitor />
+        <div
+          style={{
+            textAlign: "center",
+            fontSize: "50px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+            color: "white",
+          }}
+        >
+          Remote Energy Monitoring System
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            height: "100vh",
+          }}
+        >
+          <EnergyMonitor />
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
