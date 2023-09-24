15be3f102a4531aad75985703f7641efae0dde24
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-09-24 14:14:37 -0400

    Init Website

---
+++ Application/application/src/App.js
@@ -0,0 +1,14 @@
+import LineChart from "./Charts/LineChart";
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Remote Energy Monitoring System
+        </p>
+        <LineChart/>
+    </div>
+  );
+}
+
+export default App;
