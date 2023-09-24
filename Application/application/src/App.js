9e05074ad42c1e8aa23358060d848f850f0885de
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-09-24 15:04:04 -0400

    cooking

--- Application/application/src/App.js
+++ Application/application/src/App.js
@@ -2,11 +2,21 @@ import EnergyMonitor from "./Charts/EnergyMonitor";
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Remote Energy Monitoring System
-        </p>
-        <EnergyMonitor/>
+    <div className="App" style={{ backgroundColor: "black" }}>
+      <div style={{ textAlign: "center", fontSize: "24px", paddingTop: "30px", color:"white"}}>
+        Remote Energy Monitoring System
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <EnergyMonitor />
+      </div>
     </div>
   );
 }
--- Application/application/src/Charts/EnergyMonitor.js
+++ Application/application/src/Charts/EnergyMonitor.js
@@ -8,11 +8,11 @@ function EnergyMonitor() {
                 height: "400px",
                 border: "0",
                 scrolling: "yes",
-                marginHeight: "0",
-                marginWidth: "0",
+                marginHeight: "10",
+                marginWidth: "10",
             }
         }
-        src = "https://emoncms.org/vis/realtime?feedid=486007&colour=edc240&colourbg=ffffff&kw=1&embed=1&apikey=0345fe6c9c267a5f1c420397fbb3ed1a" >
+        src = "https://emoncms.org/vis/realtime?feedid=486007&colour=67db88&colourbg=000000&kw=1&embed=1&apikey=0345fe6c9c267a5f1c420397fbb3ed1a" >
         <
         /iframe>
     );
